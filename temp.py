891dd4bf1e76ca2e0a3f0a1436e89023d3458347
swar-oop <75157677+swar-oop@users.noreply.github.com>
2020-11-28 21:29:51 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -8,9 +8,14 @@ This is a temporary script file.
 print("HI")
 print("Hi","Swaroop", sep=' :) ', end = '***')
 print("Ok bye")
-!pip list
-import pandas
-pandas?
+#!pip list
+
 
 import pandas as pd
 pd.__version__
+
+s1= input()
+s2=input("Enter age")
+print(s2)
+
+print(f"My name is {s1} and My age is {s2}")
